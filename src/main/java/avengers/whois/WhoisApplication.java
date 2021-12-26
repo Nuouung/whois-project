67b3b4dbfa1d32d0f8013d3893952cd1d8777bba
Jinseok Lee <jin6016@gmail.com>
2021-12-27 00:30:39 +0900

    first commit

---
+++ src/main/java/avengers/whois/WhoisApplication.java
@@ -0,0 +1,13 @@
+package avengers.whois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/avengers/whois/WhoisApplicationTests.java
@@ -0,0 +1,13 @@
+package avengers.whois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
