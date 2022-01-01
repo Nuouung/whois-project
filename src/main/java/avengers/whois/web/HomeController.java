6a6be53c78b7a2544e6828cbc999a33bd584b494
Jinseok Lee <jin6016@gmail.com>
2022-01-01 23:58:36 +0900

    로그인 페이지

---
+++ src/main/java/avengers/whois/web/HomeController.java
@@ -0,0 +1,15 @@
+package avengers.whois.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String toHome() {
+        return "home";
+    }
+}
