167208adf1d473983bea18d210365dca454c9b26
Jinseok Lee <jin6016@gmail.com>
2022-01-03 11:49:35 +0900

    타임리프 적용

--- src/main/java/avengers/whois/web/HomeController.java
+++ src/main/java/avengers/whois/web/HomeController.java
@@ -1,15 +1,23 @@
 package avengers.whois.web;
 
+import avengers.whois.web.member.MemberDto;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public String toHome() {
+    public String toHome(Model model) {
+        model.addAttribute("member", new MemberDto());
         return "home";
     }
 }
