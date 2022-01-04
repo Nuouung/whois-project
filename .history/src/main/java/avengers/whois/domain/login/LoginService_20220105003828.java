d68f7c68ded3a44a741999212c3f873b6df00518
ShaunOH <dhskawn8@gmail.com>
2022-01-05 00:44:27 +0900

    [re-merged] fixed - branch commits

---
+++ .history/src/main/java/avengers/whois/domain/login/LoginService_20220105003828.java
@@ -0,0 +1,41 @@
+package avengers.whois.domain.login;
+
+import avengers.whois.web.member.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.awt.print.Pageable;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    // private final MemberRepository memberRepository;
+
+    // /**
+    // * 로그인
+    // */
+    // public Member login(String email, String password) {
+    // Optional<Member> foundMember = memberRepository.findByEmail(email);
+
+    // if (foundMember.isPresent()) {
+    // Member member = foundMember.get();
+    // if (member.getPassword().equals(password)) {
+    // return member;
+    // }
+    // }
+    // return null;
+    // }
+
+    // /**
+    // * 로그아웃
+    // */
+    // public void logout(HttpSession session) {
+    // if (session != null) {
+    // session.removeAttribute(LoginConst.loginSession);
+    // session.invalidate();
+    // }
+    // }
+}
